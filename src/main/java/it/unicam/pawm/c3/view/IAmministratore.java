70d1dfd2f9df1d9e4d08ca574bd45ee94d2f5be3
daniele-pelosi <danielepelosi99@gmail.com>
2021-03-17 23:10:37 +0100

    correzione e completamento amministrarore inclusi html relativi

--- src/main/java/it/unicam/pawm/c3/view/IAmministratore.java
+++ src/main/java/it/unicam/pawm/c3/view/IAmministratore.java
@@ -3,15 +3,16 @@ package it.unicam.pawm.c3.view;
 import it.unicam.pawm.c3.gestori.GestoreAmministratori;
 import it.unicam.pawm.c3.gestorispecifici.GestoreAccessi;
 import it.unicam.pawm.c3.merce.Categoria;
-import it.unicam.pawm.c3.personale.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -34,33 +35,35 @@ public class IAmministratore {
         gestoreAmministratori.setAmministratore(gestoreAccessi.homeAmministratore(email));
         return "home/homeAmministratore";
     }
+
     @GetMapping("/registrazioneNegozio")
     public String searchUserBbyEmailNegozio() {
         return "amministratore/searchUserByEmail";
     }
+
     @PostMapping("/addNegozioForm")
     public String addNegozioForm(String email, Model model) {
-        List<User> userList=new ArrayList<>();
-        userList.add(gestoreAmministratori.ricercaCliente(email));
-        model.addAttribute("userList",userList);
+        model.addAttribute("userList",gestoreAmministratori.searchCliente(email));
         return "amministratore/addNegozioForm";
     }
+
     @PostMapping("/addNegozio/{id}")
     public String addNegozio(@PathVariable Long id,Categoria categoria,String nomeDitta, String piva, String indirizzoRegistrazione, Model model) {
         gestoreAmministratori.registraNegozio(List.of(categoria),id,nomeDitta,piva,indirizzoRegistrazione);
         return "home/homeAmministratore";
     }
+
     @GetMapping("/registrazioneCorriere")
     public String searchUserBbyEmailCorriere() {
         return "amministratore/searchUserByEmailCorriere";
     }
+
     @PostMapping("/addCorriereForm")
     public String addCorriereForm(String email, Model model) {
-        List<User> userList=new ArrayList<>();
-        userList.add(gestoreAmministratori.ricercaCliente(email));
-        model.addAttribute("userList",userList);
+        model.addAttribute("userList",gestoreAmministratori.searchCliente(email));
         return "amministratore/addCorriereForm";
     }
+
     @PostMapping("/addCorriere/{id}")
     public String addCorriere(@PathVariable Long id,String nomeDitta, String piva, String indirizzo, Model model) {
         gestoreAmministratori.registraCorriere(id,nomeDitta,piva,indirizzo);
