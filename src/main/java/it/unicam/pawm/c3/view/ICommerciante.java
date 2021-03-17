71959bb7cf8d2ae5a8e93aaf8e23dfa73e780a76
avdil <avdil.mehmeti@studenti.unicam.it>
2021-03-17 17:09:05 +0100

    pulizia codice icommerciante

--- src/main/java/it/unicam/pawm/c3/view/ICommerciante.java
+++ src/main/java/it/unicam/pawm/c3/view/ICommerciante.java
@@ -44,29 +44,6 @@ public class ICommerciante {
 
     /******************Interfaccia GestionePromozioni***************/
 
-    public void getPromozioniAttive(){
-//        listaPromozioni.getItems().clear();
-//        listaPromozioni.getItems().addAll(gestoreCommercianti.getPromozioniAttive());
-    }
-
-    public void getMerciDoveApplicarePromozioni(){
-//        listaPromozioniPossibili.getItems().clear();
-//        listaPromozioniPossibili.getItems().addAll(gestoreCommercianti.getPromozioniPossibili());
-    }
-
-//    @FXML
-    void aggiungiPromozioneButtonEvent() {
-//        listaPromozioniPossibili.setVisible(true);
-//        dataF.setVisible(true);
-//        dataI.setVisible(true);
-//        percentualePromozione.setVisible(true);
-//        aggiungiPromozioneButton.setVisible(true);
-//        aggiuntaPromozione.setVisible(true);
-//        dataInizioPromozione.setVisible(true);
-//        dataFinePromozione.setVisible(true);
-//        scontoPromozione.setVisible(true);
-//        getMerciDoveApplicarePromozioni();
-    }
     @GetMapping("merceInPromozione")
     public String merceInPromozione(Model model) {
         model.addAttribute("minList",gestoreCommercianti.getPromozioniAttive());
@@ -98,44 +75,8 @@ public class ICommerciante {
         model.addAttribute("minList",gestoreCommercianti.getPromozioniAttive());
         return "showPromozioni";
     }
-
-
-//    @FXML
-    void addPromozioneButtonEvent(){
-//        addPromozione(listaPromozioniPossibili.getSelectionModel().getSelectedItem(),dataI.getValue(), dataF.getValue(),Double.parseDouble(percentualePromozione.getText()));
-//        getPromozioniAttive();
-//        initFieldPromozioni();
-    }
-//    @FXML
-    void rimuoviPromozioneButtonEvent() {
-//        listaPromozioniPossibili.setVisible(false);
-//        dataF.setVisible(false);
-//        dataI.setVisible(false);
-//        percentualePromozione.setVisible(false);
-//        aggiuntaPromozione.setVisible(false);
-//        rimuoviPromozione(listaPromozioni.getSelectionModel().getSelectedItems());
-//        getPromozioniAttive();
-    }
-
-    public void initFieldPromozioni(){
-//        listaPromozioni.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-//        listaPromozioniPossibili.setVisible(false);
-//        dataF.setVisible(false);
-//        dataI.setVisible(false);
-//        percentualePromozione.setVisible(false);
-//        aggiuntaPromozione.setVisible(false);
-//        dataInizioPromozione.setVisible(false);
-//        dataFinePromozione.setVisible(false);
-//        scontoPromozione.setVisible(false);
-//        getPromozioniAttive();
-    }
-
     /******************Interfaccia Gestione Corriere***************/
 
-    public void getCorrieri() {
-//        corrieriDaAggiungere.getItems().clear();
-//        corrieriDaAggiungere.getItems().addAll(gestoreCommercianti.getCorrieri());
-    }
     @GetMapping("showCorrieriDaAggiungere")
     public String showCorrieriDaAggiunguere(Model model)
     {
@@ -148,19 +89,8 @@ public class ICommerciante {
         model.addAttribute("corrieriList",gestoreCommercianti.getCorrieri());
         return "showCorrieriDaAggiungere";
     }
-//    @FXML
-    void confermAggiuntaCorriereButtonEvent() {
-//        addCorrieri(corrieriDaAggiungere.getSelectionModel().getSelectedItems());
-//        getCorrieri();
-    }
 
     /******************Interfaccia assunzione addetto***************/
-
-    public void getClienti(String email){
-//        clientiFiltratiAA.getItems().clear();
-//        clientiFiltratiAA.getItems().addAll(gestoreCommercianti.getCliente(email));
-    }
-    //METODO CHE SUL GESTORE SI CHIAMA GETCLIENTE(String email)
 
     @GetMapping("assunzioneAddetto")
     public String assunzioneForm() {
@@ -180,80 +110,7 @@ public class ICommerciante {
          gestoreCommercianti.assunzioneAddetto(id);
         return "assunzioneAddetto";
     }
-    public void assunzioneAddetto(Cliente cliente){
-//        gestoreCommercianti.assunzioneAddetto(cliente);
-    }
-
-//    @FXML
-    void cercaClienteAAButtonEvent() {
-//        getClienti(emailAA.getText());
-    }
-
-//    @FXML
-    void confermaAssunzioneAddettoButtonEvent() {
-//        assunzioneAddetto(clientiFiltratiAA.getSelectionModel().getSelectedItem());
-//        clientiFiltratiAA.getItems().clear();
-    }
-
     /****************Gestione inventario**************/
-
-    private void setVisibleAggiunta(boolean flag){
-//        nomeMerceGI.clear();
-//        descrizioneMerceGI.clear();
-//        quantitaMerceGI.clear();
-//        prezzoMerceGI.clear();
-//        scontoMerceGI.clear();
-//        categoriaMerceGI.getItems().clear();
-//        categoriaMerceGI.getItems().addAll(Categoria.values());
-//        nomeLabelGI.setVisible(flag);
-//        nomeMerceGI.setVisible(flag);
-//        quantitaLabelGI.setVisible(flag);
-//        quantitaMerceGI.setVisible(flag);
-//        prezzoLabelGI.setVisible(flag);
-//        prezzoMerceGI.setVisible(flag);
-//        scontoLabelGI.setVisible(flag);
-//        scontoMerceGI.setVisible(flag);
-//        confermaAggiuntaMerceButton.setVisible(flag);
-    }
-
-    private void setVisibileModifica(boolean flag){
-//        quantitaMerceGI.clear();
-//        prezzoMerceGI.clear();
-//        scontoMerceGI.clear();
-//        quantitaLabelGI.setVisible(flag);
-//        quantitaMerceGI.setVisible(flag);
-//        prezzoLabelGI.setVisible(flag);
-//        prezzoMerceGI.setVisible(flag);
-//        scontoLabelGI.setVisible(flag);
-//        scontoMerceGI.setVisible(flag);
-//        confermaModificaButton.setVisible(flag);
-    }
-
-    private void setVisibileRimozione(boolean flag){
-//        quantitaMerceGI.clear();
-//        quantitaLabelGI.setVisible(flag);
-//        quantitaMerceGI.setVisible(flag);
-//        confermaRimozioneMerceButton.setVisible(flag);
-    }
-
-    public void initGestioneInventario() {
-//        getMerciInventario();
-//        setVisibleAggiunta(false);
-//        setVisibileRimozione(false);
-//        setVisibileModifica(false);
-//        idLabel.setVisible(false);
-//        idMerceGI.setVisible(false);
-//        descrizioneLabelGI.setVisible(false);
-//        descrizioneMerceGI.setVisible(false);
-//        categoriaLabelGI.setVisible(false);
-//        categoriaMerceGI.setVisible(false);
-//        verificaIdMerceButton.setVisible(false);
-    }
-
-    public void getMerciInventario() {
-//        merciInventario.getItems().clear();
-//        merciInventario.getItems().addAll(gestoreCommercianti.getInventario());
-    }
     @GetMapping("showInventario")
     public String showInventario(Model model) {
         model.addAttribute("minList",gestoreCommercianti.getInventario());
@@ -300,87 +157,5 @@ public class ICommerciante {
         gestoreCommercianti.modificaMerce(id,prezzo,sconto,quantita);
         model.addAttribute("minList",gestoreCommercianti.getInventario());
         return "showInventario";
-    }
-    private void aggiungiMerce(Long id, String nome, String descrizione, Categoria categoria, double quantita, double prezzo, double sconto) {
-        gestoreCommercianti.addMerce(id,nome,descrizione,categoria,quantita,prezzo,sconto);
-    }
-    private boolean verificaIdMerce(Long id){
-        return gestoreCommercianti.verificaIdMerce(id);
-    }
-
-
-//    @FXML
-    void verificaIdMerceButtonEvent(){
-//        if(verificaIdMerce(Long.parseLong(idMerceGI.getText()))){
-//            setVisibleAggiunta(true);
-//            nomeLabelGI.setVisible(false);
-//            nomeMerceGI.setVisible(false);
-//            verificaIdMerceButton.setVisible(false);
-//        } else {
-//            setVisibleAggiunta(true);
-//            verificaIdMerceButton.setVisible(false);
-//            descrizioneLabelGI.setVisible(true);
-//            descrizioneMerceGI.setVisible(true);
-//            categoriaLabelGI.setVisible(true);
-//            categoriaMerceGI.setVisible(true);
-//        }
-    }
-
-//    @FXML
-    void aggiungiButtonEvent() {
-//        setVisibileRimozione(false);
-//        setVisibileModifica(false);
-//        idLabel.setVisible(true);
-//        idMerceGI.setVisible(true);
-//        verificaIdMerceButton.setVisible(true);
-    }
-
-//    @FXML
-    void modificaMerceButtonEvent(){
-        setVisibileRimozione(false);
-        setVisibleAggiunta(false);
-        setVisibileModifica(true);
-    }
-
-//    @FXML
-    void rimuoviMerceButtonEvent() {
-        setVisibleAggiunta(false);
-        setVisibileModifica(false);
-        setVisibileRimozione(true);
-    }
-
-//    @FXML
-    void confermaAggiuntaMerceButtonEvent() {
-//        aggiungiMerce(Long.parseLong(idMerceGI.getText()),nomeMerceGI.getText(), descrizioneMerceGI.getText(), categoriaMerceGI.getValue(), Double.parseDouble(quantitaMerceGI.getText()), Double.parseDouble(prezzoMerceGI.getText()), Double.parseDouble(scontoMerceGI.getText()));
-//        initGestioneInventario();
-//        setVisibleAggiunta(false);
-//        idMerceGI.clear();
-//        idLabel.setVisible(false);
-//        verificaIdMerceButton.setVisible(false);
-//        idMerceGI.setVisible(false);
-//        descrizioneMerceGI.clear();
-//        categoriaMerceGI.getItems().clear();
-//        descrizioneLabelGI.setVisible(false);
-//        descrizioneMerceGI.setVisible(false);
-//        categoriaLabelGI.setVisible(false);
-//        categoriaMerceGI.setVisible(false);
-    }
-
-//    @FXML
-    void confermaRimozioneMerceButtonEvent() {
-//        rimuoviMerce(merciInventario.getSelectionModel().getSelectedItems(),Double.parseDouble(quantitaMerceGI.getText()));
-//        initGestioneInventario();
-//        setVisibileRimozione(false);
-    }
-
-//    @FXML
-    void confermaModificaButtonEvent(){
-//        modificaMerce(merciInventario.getSelectionModel().getSelectedItem(),Double.parseDouble(prezzoMerceGI.getText()), Double.parseDouble(scontoMerceGI.getText()), Double.parseDouble(quantitaMerceGI.getText()));
-//        initGestioneInventario();
-//        setVisibileModifica(false);
-    }
-
-    public void setGestoreCommerciante(GestoreCommercianti gestoreCommercianti) {
-        this.gestoreCommercianti = gestoreCommercianti;
     }
 }
