e186d6e091166bc53463d906ffd3523871db761c
daniele-pelosi <danielepelosi99@gmail.com>
2021-03-21 21:54:26 +0100

    revisione html vari generale e spostamento consulta promozioni in un'unica pagina

--- src/main/java/it/unicam/pawm/c3/view/ICliente.java
+++ src/main/java/it/unicam/pawm/c3/view/ICliente.java
@@ -3,7 +3,7 @@ package it.unicam.pawm.c3.view;
 import it.unicam.pawm.c3.gestori.GestoreClienti;
 import it.unicam.pawm.c3.gestorispecifici.GestoreAccessi;
 import it.unicam.pawm.c3.merce.Categoria;
-import it.unicam.pawm.c3.vendita.VenditaSpedita;
+import it.unicam.pawm.c3.merce.MerceInventarioNegozio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -41,13 +42,21 @@ public class ICliente {
     @GetMapping("promozione")
     public String selezionaCategoria(Model model) {
         model.addAttribute("minList",gestoreClienti.getPromozioni());
-        return "cliente/showCategoriePromozioni";
+        return "cliente/showPromozioni";
     }
 
     @PostMapping("promozioniFiltrate")
     public String filtraPromozioni(Categoria categoria, Model model){
+        List<MerceInventarioNegozio> list = new ArrayList<>();
+        model.addAttribute("minList" , list);
         model.addAttribute("minList",gestoreClienti.filtraPromozioniPerCategoria(categoria));
-        return "cliente/promozioniPerCategoria";
+        return "cliente/showPromozioni";
+    }
+
+    @GetMapping("azzeraFiltro")
+    public String azzeraFiltroPromozioni(Model model){
+        model.addAttribute("minList",gestoreClienti.getPromozioni());
+        return "cliente/showPromozioni";
     }
     /*****************Ricerca Prodotto******************/
     @GetMapping("ricercaProdottoForm")
